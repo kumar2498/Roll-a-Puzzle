11d628d95fb62ac3eff0012c3af0c72dc6fa4908
240S-official <240S-official@users.noreply.github.com>
2018-05-27 12:40:04 +0530

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	public GameObject ball;
+
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+
+		offset = transform.position - ball.transform.position; 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = offset + ball.transform.position;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	private Rigidbody rb; 
+	public int speed = 10;
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is ca	lled once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("CubeObjects"))
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+
+
+}
